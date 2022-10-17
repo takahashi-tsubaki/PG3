35324a356c37b25e4a83d07d38d7abcc339f2849
LC1B_15_takahashi_tsubaki <k021g1132@g.neec.ac.jp>
2022-10-17 10:08:03 +0900

    関数は作ったがerrorが発生

--- main.cpp
+++ main.cpp
@@ -1,65 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
-//再帰的な賃金体系関数
-int Recursive(int n,int hour,int maxHour)
+#include <random>
+int RandomNumber(int num)
 {
-	//最初の1時間
-	if (hour == 1)
+	if (num % 2 == 0)
 	{
-		return n;
+		num = rand() + 1;
 	}
 	else
 	{
-		//2時間目以降の時給
-		for (int i = 1; i < hour; i++)
-		{
-			n = n+((n * 2) - 50);
-		}
-
+		num = rand();
 	}
-	
-	return n;
+
+	return num;
 }
 
-//比較関数
-void comparison(int flat,int rec)
+int Scanf(int num)
 {
-	//一般時給と再帰的な時給を比較する
-	int n = flat - rec;
-	//絶対値をとる
-	n = abs(n);
-	if (flat > rec)
+	char str[5];
+	if (scanf("%s", str) == '偶数')
 	{
-		printf("一般のほうが%d円高い。\n\n",n);
+		num %= 0;
+	}
+	else if(scanf("%s", str) == '奇数')
+	{
+		num %= 1;
 	}
 	else
 	{
-		printf("再帰的のほうが%d円高い。\n\n", n);
+		printf("もう一度入力してください");
 	}
+	return num;
 }
 
 int main()
 {
 	
-	int n = 100;//
-	int hour = 1;//時間
-	int max = 8;//最大
-	int result;//結果
+	int outputNum = 0;
 
-	//一般体系賃金
-	int flatWage = 1072*hour;
-	
-	result = Recursive(n, hour, max);
-	for (int i = 1; i < max; i++)
-	{
-		flatWage = 1072 * hour;
-		
-		printf("一般   : %d時間の給料 = %d\n", hour, flatWage);
-		printf("再帰的 : %d時間の給料 = %d\n", hour, result);
-		hour++;
+	int(*calc)(int);
 
-		comparison(flatWage, result);
-	}
+	calc = Scanf;
+	printf("%d", calc(outputNum));
+
+	calc = RandomNumber;
+	printf("%d", calc(outputNum));
+
+
+	printf("main = %p\n", main);
 	
 	return 0;
 }
