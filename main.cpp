03225377fd0b22b545efe6067ea51e01e70bb6a6
LC1B_15_takahashi_tsubaki <k021g1132@g.neec.ac.jp>
2022-12-05 12:33:16 +0900

    再起関数の編集

--- main.cpp
+++ main.cpp
@@ -1,56 +1,37 @@
 #include <stdio.h>
-#include <stdlib.h>
 
+int RecursiveSalary(int hour) {
+    if (hour <= 1) {
+        return (100);
+    }
 
-//再帰的な賃金体系関数
-int RecursiveSalary(int salary,int previous)
-{
-	if (previous < salary)
-	{
-		return salary;
-	}
-	return RecursiveSalary(salary*2-50, previous);
-}
-//比較関数
-void comparison()
-{
-	int rec = 100;//
-	int oldRec = 0;
-	int oneDays = 24;
-	//一般体系賃金
-	int flat = 1072;
-	int totalFlat = 0;
-	//比較
-	for (int i = 1; i < oneDays; i++)
-	{
-		totalFlat = flat*i;
-		oldRec = RecursiveSalary(rec, oldRec);
-		printf("一般　 : %d時間の給料 = %d\n", i, totalFlat);
-		printf("再帰的 : %d時間の給料 = %d\n", i, oldRec);
-
-		//一般時給と再帰的な時給を比較する
-		int n = totalFlat - oldRec;
-		//絶対値をとる
-		n = abs(n);
-		if (totalFlat > oldRec)
-		{
-			printf("一般のほうが%d円高い。\n\n", n);
-		}
-		else
-		{
-			printf("再帰的のほうが%d円高い。\n\n", n);
-			printf("%d時間で再帰的な賃金が上回る。\n\n", i);
-			//再帰的のほうが上回った場合は比較をやめる
-			break;
-		}
-
-	}
-	
+    return (RecursiveSalary(hour - 1) * 2 - 50);
 }
 
-int main()
-{
-	//賃金比較
-	comparison();
-	return 0;
+void Result(int hour) {
+    int rec = 0;
+    int flat = 0;
+
+    flat = 1072 * hour;
+
+    for (int i = 0; i < hour; i++) {
+        rec += RecursiveSalary(i + 1);
+    }
+
+    printf("再帰的の賃金 : %d円  ", rec);
+    printf("一般の賃金 : %d円\n\n",flat);
+
+    if (rec > flat) {
+        printf("再帰的賃金が超えた瞬間 = %d時間\n",hour);
+    }
+
+}
+
+
+
+int main() {
+    int hour = 8;
+    Result(hour);
+
+    return 0;
 }
