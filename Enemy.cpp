1d5601036f76d933697e860eeb5c4fa7e4baf875
LC1B_15_takahashi_tsubaki <k021g1132@g.neec.ac.jp>
2023-01-23 10:02:39 +0900

    Enemy.hと.Cppの実装

---
+++ Enemy.cpp
@@ -0,0 +1,16 @@
+#include "Enemy.h"
+
+void Enemy::Initialize()
+{
+
+}
+
+void Enemy::Update()
+{
+
+}
+
+void Enemy::Destroy()
+{
+
+}
---
+++ Enemy.h
@@ -0,0 +1,19 @@
+#pragma once
+class Enemy
+{
+public://メンバ関数
+
+	void Initialize();
+
+	void Update();
+
+	void Destroy();
+
+public://情的メンバ変数
+
+	static int EnemyCount;
+
+private://メンバ変数
+
+};
+
